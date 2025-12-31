f72d32a079647e9ab8f42d4eb5caf9cac6ac2c58
jaguda85 <spanie-czysto0n@icloud.com>
2025-12-31 12:21:25 +0100

    wgrywanie ustawien

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://github.com/jaguda85',
+  base: '/esp32-projects',
+});
