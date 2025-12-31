1f11016609234080d4e1e4c42a73849180414b18
jaguda85 <spanie-czysto0n@icloud.com>
2025-12-31 13:30:16 +0100

    Dodano vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jaguda85.github.io',
   base: '/esp32-projects',
+  adapter: vercel(),
 });
